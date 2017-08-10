3cf17abe4ff87ed96551954471c352c4d8ddfd51
Andrew Franklin <andrew.franklin@inl.gov>
2017-08-10 10:31:27 -0600

    Added view factor calculation for finite plates.

--- thermal_radiation/view_factors.py
+++ thermal_radiation/view_factors.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, pi, atan, log
 
 def two_infintely_long_plates(w1, w2, d):
     """
@@ -34,3 +34,53 @@ def two_infintely_long_plates(w1, w2, d):
     t2 = sqrt(((C - B)**2) + 4)
 
     return (1.0 / (2.0 * B)) * (t1 - t2)
+
+def two_coaxial_parallel_plates(l1, l2, d, apprx=False):
+    """
+    Calculates the view factor from plate 1 to plate 2 where both plates are
+    finte and square. The centerlines of each plate are lined up.
+
+                                          plate 2
+                              plate 1        _   ___
+                           ___   _          | |   A
+                            A   | |         | |   |
+                            |   | |         | |   |
+                            l1  | |<-- d -->| |   l2
+                            |   | |         | |   |
+                           _V_  |_|         | |   |
+                                            |_|  _V_
+
+    Args:
+        l1 (float): The side length of the first plate
+        l2 (float): The side length of the second plate
+        d (float): The distance between the two plates
+
+    Returns:
+        float: The view factor from plate 1 to plate 2.
+    """
+    assert l1 > 0.0
+    assert l2 > 0.0
+    assert d > 0.0
+
+    A = l1 / d
+    B = l2 / l1
+
+    if apprx and A < 0.2:
+        return ((A * B) ** 2) / pi
+
+    else:
+        X = A * (1 + B)
+        Y = A * (1 - B)
+        tmp = (A * A * (1 + (B * B))) + 2
+        X4 = sqrt((X * X) + 4)
+        Y4 = sqrt((Y * Y) + 4)
+
+        denom = ((Y * Y) + 2) * ((X * X) + 2)
+        f12  = log((tmp * tmp) / denom)
+        f12 += Y4 * ((Y * atan(Y / Y4)) - (X * atan(X / Y4)))
+        f12 += X4 * ((X * atan(X / X4)) - (Y * atan(Y / X4)))
+        return f12 / (pi * A * A)
+
+
+if __name__ == '__main__':
+    print(two_coaxial_parallel_plates(2.0, 2.0, 1.0, apprx=False))
